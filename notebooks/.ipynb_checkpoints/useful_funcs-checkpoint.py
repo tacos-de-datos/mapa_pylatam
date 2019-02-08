5771113ccd05890afc8d4170160a5e52caf1d04f
chekos <chekos.data@gmail.com>
2019-02-08 12:25:03 -0800

    Delete useful_funcs-checkpoint.py

--- notebooks/.ipynb_checkpoints/useful_funcs-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# A collection of useful functions to use in this proyect
-
-# from realpython.com
-def tree(directory):
-    print(f'+ {directory}')
-    for path in sorted(directory.rglob('*')):
-        depth = len(path.relative_to(directory).parts)
-        spacer = '    ' * depth
-        print(f'{spacer}+ {path.name}')
